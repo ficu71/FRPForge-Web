14538b771d7af8e3c708e603ddf3a9f89fdf3e08
ficu71 <lodygi-bloczek.8p@icloud.com>
2025-06-27 00:22:38 +0200

    test_vendors.py

--- tests/test_vendors.py
+++ tests/test_vendors.py
@@ -1,1 +1,13 @@
-# test_vendors
+import unittest
+from vendors.loader import VendorLoader
+
+class TestVendors(unittest.TestCase):
+    def test_load_vendors(self):
+        vendors = VendorLoader.load_vendors()
+        self.assertIn("samsung", vendors)
+        self.assertIn("xiaomi", vendors)
+        self.assertIn("huawei", vendors)
+        self.assertIn("oppo", vendors)
+
+if __name__ == '__main__':
+    unittest.main()
