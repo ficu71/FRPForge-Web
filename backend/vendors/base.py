f17a81577e936eabd690bed04c60ceb15d88ff84
ficu71 <lodygi-bloczek.8p@icloud.com>
2025-06-27 00:10:01 +0200

    base.py

--- backend/vendors/base.py
+++ backend/vendors/base.py
@@ -1,1 +1,10 @@
-# BaseBypass class
+class BaseBypass:
+    def __init__(self, device_id):
+        self.device_id = device_id
+
+    def execute(self):
+        raise NotImplementedError("Metoda execute musi być zaimplementowana przez podklasę")
+
+    @staticmethod
+    def demo_steps():
+        raise NotImplementedError("Metoda demo_steps musi być zaimplementowana przez podklasę")
