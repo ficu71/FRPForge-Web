213310f23c9ad0ffd45a83fc31abb6500f491e58
ficu71 <lodygi-bloczek.8p@icloud.com>
2025-06-27 00:23:15 +0200

    test_webusb.py

--- tests/test_webusb.py
+++ tests/test_webusb.py
@@ -1,1 +1,12 @@
-# test_webusb
+import unittest
+from utils.webusb import WebUSBHandler
+
+class TestWebUSB(unittest.TestCase):
+    def test_get_devices(self):
+        webusb = WebUSBHandler()
+        devices = webusb.get_devices()
+        self.assertGreater(len(devices), 0)
+        self.assertEqual(devices[0]["vendor"], "Samsung")
+
+if __name__ == '__main__':
+    unittest.main()
